74c7363eb3e9b2e40c3a4bf7271eb3190b58dae0
yizhikaohongshu <840221557@qq.com>
2025-05-29 11:04:05 +0800

    add stl_algobase.h

--- stl/stl_algobase.h
+++ stl/stl_algobase.h
@@ -6,234 +6,278 @@
 #include "stl_pair.h"
 #include <cstring>
 
-template<typename InputIterator1, typename InputIterator2>
-inline bool equal(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2)
+template<typename _InputIterator1, typename _InputIterator2>
+inline bool equal(_InputIterator1 __first1, _InputIterator1 __last1, _InputIterator2 __first2)
 {
-    for (; first1 != last1; ++first1, ++first2) {
-        if (*first1 != *first2) {
+    for (; __first1 != __last1; ++__first1, ++__first2) {
+        if (*__first1 != *__first2) {
             return false;
         }
     }
     return true;
 }
 
-template<typename InputIterator1, typename InputIterator2, typename BinaryPredicate>
-inline bool equal(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, BinaryPredicate binary_pred)
+template<typename _InputIterator1, typename _InputIterator2, typename _BinaryPredicate>
+inline bool equal(_InputIterator1 __first1, _InputIterator1 __last1, _InputIterator2 __first2, _BinaryPredicate __binary_pred)
 {
-    for (; first1 != last1; ++first1, ++first2) {
-        if (!binary_pred(*first1, *first2)) {
+    for (; __first1 != __last1; ++__first1, ++__first2) {
+        if (!__binary_pred(*__first1, *__first2)) {
             return false;
         }
     }
     return true;
 }
 
-template<typename ForwardIterator, typename T>
-void fill(ForwardIterator first, ForwardIterator last, const T& value)
+template<typename _ForwardIterator, typename _Tp>
+void fill(_ForwardIterator __first, _ForwardIterator __last, const _Tp& __value)
 {
-    for (; first != last; ++first) {
-        *first = value;
+    for (; __first != __last; ++__first) {
+        *__first = __value;
     }
 }
 
-template<typename OutputIterator, typename Size, typename T>
-OutputIterator fill_n(OutputIterator first, Size n, const T& value)
+template<typename _OutputIterator, typename _Size, typename _Tp>
+_OutputIterator fill_n(_OutputIterator __first, _Size __n, const _Tp& __value)
 {
-    for (; n > 0; --n, ++first) {
-        *first = value;
+    for (; __n > 0; --__n, ++__first) {
+        *__first = __value;
     }
-    return first;
+    return __first;
 }
 
-template<typename ForwardIterator1, typename ForwardIterator2, typename T>
-inline void __iter_swap(ForwardIterator1 a, ForwardIterator2 b, T*)
+template<typename _ForwardIterator1, typename _ForwardIterator2, typename _Tp>
+inline void __iter_swap(_ForwardIterator1 __a, _ForwardIterator2 __b, _Tp*)
 {
-    T tmp   = a;
-    a       = b;
-    b       = tmp;
+    _Tp __tmp   = __a;
+    __a         = __b;
+    __b         = __tmp;
 }
 
-template<typename ForwardIterator1, typename ForwardIterator2>
-inline void iter_swap(ForwardIterator1 a, ForwardIterator2 b)
+template<typename _ForwardIterator1, typename _ForwardIterator2>
+inline void iter_swap(_ForwardIterator1 __a, _ForwardIterator2 __b)
 {
-    // typename iterator_tratis<ForwardIterator1>::value_type tmp = *a;
+    // typename iterator_tratis<_ForwardIterator1>::value_type tmp = *a;
     // *a = *b;
     // *b = tmp;
-    __iter_swap(a, b, value_type(a));
+    __iter_swap(__a, __b, value_type(__a));
 }
 
-template<typename T>
-inline const T& max(const T& a, const T& b)
+template<typename _Tp>
+inline const _Tp& max(const _Tp& __a, const _Tp& __b)
 {
-    return b < a ? a : b;
+    return __b < __a ? __a : __b;
 }
 
-template<typename T, typename Compare>
-inline const T& max(const T& a, const T& b, Compare comp)
+template<typename _Tp, typename _Compare>
+inline const _Tp& max(const _Tp& __a, const _Tp& __b, _Compare __comp)
 {
-    return comp(b, a) ? a : b;
+    return __comp(__b, __a) ? __a : __b;
 }
 
-template<typename T>
-inline const T& min(const T& a, const T& b)
+template<typename _Tp>
+inline const _Tp& min(const _Tp& __a, const _Tp& __b)
 {
-    return a < b ? a : b;
+    return __a < __b ? __a : __b;
 }
 
-template<typename T, typename Compare>
-inline const T& min(const T& a, const T& b, Compare comp)
+template<typename _Tp, typename _Compare>
+inline const _Tp& min(const _Tp& __a, const _Tp& __b, _Compare __comp)
 {
-    return comp(a, b) ? a : b;
+    return __comp(__a, __b) ? __a : __b;
 }
 
 // TODO
-template<typename InputIterator1, typename InputIterator2>
-pair<InputIterator1, InputIterator2> mismatch(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2)
+template<typename _InputIterator1, typename _InputIterator2>
+pair<_InputIterator1, _InputIterator2> mismatch(_InputIterator1 __first1, _InputIterator1 __last1, _InputIterator2 __first2)
 {
-    while (first1 != last1 && *first1 == *first2) {
-        ++first1;
-        ++first2;
+    while (__first1 != __last1 && *__first1 == *__first2) {
+        ++__first1;
+        ++__first2;
     }
-    return pair<InputIterator1, InputIterator2>(first1, first2);
+    return pair<_InputIterator1, _InputIterator2>(__first1, __first2);
 }
 
-template<typename InputIterator1, typename InputIterator2, typename BinaryPredicate>
-pair<InputIterator1, InputIterator2> mismatch(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, BinaryPredicate binary_pred)
+template<typename _InputIterator1, typename _InputIterator2, typename _BinaryPredicate>
+pair<_InputIterator1, _InputIterator2> mismatch(_InputIterator1 __first1, _InputIterator1 __last1, _InputIterator2 __first2, _BinaryPredicate __binary_pred)
 {
-    while (first1 != last1 && binary_pred(*first1, *first2)) {
-        ++first1;
-        ++first2;
+    while (__first1 != __last1 && __binary_pred(*__first1, *__first2)) {
+        ++__first1;
+        ++__first2;
     }
-    return pair<InputIterator1, InputIterator2>(first1, first2);
+    return pair<_InputIterator1, _InputIterator2>(__first1, __first2);
 }
 
-template<typename T>
-inline void swap(T& a, T& b)
+template<typename _Tp>
+inline void swap(_Tp& __a, _Tp& __b)
 {
-    T tmp   = a;
-    a       = b;
-    b       = tmp;
+    _Tp __tmp   = __a;
+    __a         = __b;
+    __b         = __tmp;
 }
 
-template<typename InputIterator1, typename InputIterator2>
-bool lexicographical_compare(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, InputIterator2 last2)
+template<typename _InputIterator1, typename _InputIterator2>
+bool lexicographical_compare(_InputIterator1 __first1, _InputIterator1 __last1, _InputIterator2 __first2, _InputIterator2 __last2)
 {
-    for (; first1 != last1 && first2 != last2; ++first1, ++first2) {
-        if (*first1 < *first2) {
+    for (; __first1 != __last1 && __first2 != __last2; ++__first1, ++__first2) {
+        if (*__first1 < *__first2) {
             return true;
         }
-        if (*first2 < *first1) {
+        if (*__first2 < *__first1) {
             return false;
         }
     }
 
-    return first1 == last1 && first2 != last2;
+    return __first1 == __last1 && __first2 != __last2;
 }
 
-template<typename InputIterator1, typename InputIterator2, typename Compare>
-bool lexicographical_compare(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2, InputIterator2 last2, Compare comp)
+template<typename _InputIterator1, typename _InputIterator2, typename _Compare>
+bool lexicographical_compare(_InputIterator1 __first1, _InputIterator1 __last1, _InputIterator2 __first2, _InputIterator2 __last2, _Compare __comp)
 {
-    for (; first1 != last1 && first2 != last2; ++first1, ++first2) {
-        if (comp(*first1, *first2)) {
+    for (; __first1 != __last1 && __first2 != __last2; ++__first1, ++__first2) {
+        if (__comp(*__first1, *__first2)) {
             return true;
         }
-        if (comp(*first2, *first1)) {
+        if (__comp(*__first2, *__first1)) {
             return false;
         }
     }
 
-    return first1 == last1 && first2 != last2;
+    return __first1 == __last1 && __first2 != __last2;
 } 
 
-template<typename RandomAccessIterator, typename OutputIterator, typename Distance>
-inline OutputIterator __copy_d(RandomAccessIterator first, RandomAccessIterator last, OutputIterator result, Distance*)
+template<typename _RandomAccessIterator, typename _OutputIterator, typename _Distance>
+inline _OutputIterator __copy_d(_RandomAccessIterator __first, _RandomAccessIterator __last, _OutputIterator __result, _Distance*)
 {
     // 以 n 决定循环的执行次数, 速度快
-    for (Distance n = last - first; n > 0; --n, ++result, ++first) {
-        *result = *first;
+    for (_Distance __n = __last - __first; __n > 0; --__n, ++__result, ++__first) {
+        *__result = *__first;
     }
-    return result;
+    return __result;
 }
 
-template<typename T>
-inline T* __copy_t(const T* first, const T* last, T* result, __true_type)
+template<typename _Tp>
+inline _Tp* __copy_t(const _Tp* __first, const _Tp* __last, _Tp* __result, __true_type)
 {
-    memmove(result, first, sizeof(T) * (last - first));
-    return result + (last - first);
+    memmove(__result, __first, sizeof(_Tp) * (__last - __first));
+    return __result + (__last - __first);
 }
 
-template<typename T>
-inline T* __copy_t(const T* first, const T* last, T* result, __false_type)
+template<typename _Tp>
+inline _Tp* __copy_t(const _Tp* __first, const _Tp* __last, _Tp* __result, __false_type)
 {
-    return __copy_d(first, last, result, (ptrdiff_t*) nullptr);
+    return __copy_d(__first, __last, __result, (ptrdiff_t*) nullptr);
 }
 
-template<typename InputIterator, typename OutputIterator>
-inline OutputIterator __copy(InputIterator first, InputIterator last, OutputIterator result, input_iterator_tag)
+template<typename _InputIterator, typename _OutputIterator>
+inline _OutputIterator __copy(_InputIterator __first, _InputIterator __last, _OutputIterator __result, input_iterator_tag)
 {
     // 以迭代器等同与否, 决定循环是否继续, 速度慢
-    for (; first != last; ++result, ++first) {
-        *result = *first;
+    for (; __first != __last; ++__result, ++__first) {
+        *__result = *__first;
     }
-    return result;
+    return __result;
 }
 
-template<typename RandomAccessIterator, typename OutputIterator>
-inline OutputIterator __copy(RandomAccessIterator first, RandomAccessIterator last, OutputIterator result, random_access_iterator_tag)
+template<typename _RandomAccessIterator, typename _OutputIterator>
+inline _OutputIterator __copy(_RandomAccessIterator __first, _RandomAccessIterator __last, _OutputIterator __result, random_access_iterator_tag)
 {
-    return __copy_d(first, last, result, distance_type(first));
+    return __copy_d(__first, __last, __result, distance_type(__first));
 }
 
 // 完全泛化版本
-template<typename InputIterator, typename OutputIterator>
+template<typename _InputIterator, typename _OutputIterator>
 struct __copy_dispatch
 {
-    OutputIterator operator()(InputIterator first, InputIterator last, OutputIterator result)
+    _OutputIterator operator()(_InputIterator __first, _InputIterator __last, _OutputIterator __result)
     {
-        return __copy(first, last, result, iterator_category(first));
+        return __copy(__first, __last, __result, iterator_category(__first));
     }
 };
 
-// 偏特化版本, 两个参数都是 T* 指针形式
-template<typename T>
-struct __copy_dispatch<T*, T*>
+// 偏特化版本, 两个参数都是 _Tp* 指针形式
+template<typename _Tp>
+struct __copy_dispatch<_Tp*, _Tp*>
 {
-    T* operator()(T* first, T* last, T* result) 
+    _Tp* operator()(_Tp* __first, _Tp* __last, _Tp* __result) 
     {
-        typedef typename __type_traits<T>::has_trivial_assignment_operator t;
-        return __copy_t(first, last, result, t());
+        typedef typename __type_traits<_Tp>::has_trivial_assignment_operator t;
+        return __copy_t(__first, __last, __result, t());
     }
 };
 
-// 偏特化版本, 第一个参数为 const T*指针形式, 第二个参数为 T* 指针形式
-template<typename T>
-struct __copy_dispatch<const T*, T*>
+// 偏特化版本, 第一个参数为 const _Tp*指针形式, 第二个参数为 _Tp* 指针形式
+template<typename _Tp>
+struct __copy_dispatch<const _Tp*, _Tp*>
 {
-    T* operator()(const T* first, const T* last, T* result)
+    _Tp* operator()(const _Tp* __first, const _Tp* __last, _Tp* __result)
     {
-        typedef typename __type_traits<T>::has_trivial_assignment_operator t;
-        return __copy_t(first, last, result, t());
+        typedef typename __type_traits<_Tp>::has_trivial_assignment_operator t;
+        return __copy_t(__first, __last, __result, t());
     }
 };
 
-template<typename InputIterator, typename OutputIterator>
-inline OutputIterator copy(InputIterator first, InputIterator last, OutputIterator result)
+template<typename _InputIterator, typename _OutputIterator>
+inline _OutputIterator copy(_InputIterator __first, _InputIterator __last, _OutputIterator __result)
 {
-    return __copy_dispatch<InputIterator, OutputIterator>()(first, last, result);
+    return __copy_dispatch<_InputIterator, _OutputIterator>()(__first, __last, __result);
 }
 
 // 重载
-inline char* copy(const char* first, const char* last, char* result)
+inline char* copy(const char* __first, const char* __last, char* __result)
 {
-    memmove(result, first, last - first);
-    return result + (last - first);
+    memmove(__result, __first, __last - __first);
+    return __result + (__last - __first);
 }
 
 // 重载
-inline wchar_t* copy(const wchar_t* first, const wchar_t* last, wchar_t* result)
+inline wchar_t* copy(const wchar_t* __first, const wchar_t* __last, wchar_t* __result)
 {
-    memmove(result, first, sizeof(wchar_t) * (last - first));
-    return result + (last - first);
+    memmove(__result, __first, sizeof(wchar_t) * (__last - __first));
+    return __result + (__last - __first);
 }
+
+// copy_backward
+template<typename _BidirectionalIterator1, typename _BidirectionalIterator2, typename _Distance>
+inline _BidirectionalIterator2 __copy_backward(_BidirectionalIterator1 __first, _BidirectionalIterator1 __last, _BidirectionalIterator2 __result, bidirectional_iterator_tag, _Distance*)
+{
+    while (__first != __last) {
+        *--__result = *--__last;
+    }
+    return __result;
+}
+
+template<typename _RandomAccessIterator, typename _BidirectionalIterator, typename _Distance>
+inline _BidirectionalIterator __copy_backward(_RandomAccessIterator __first, _RandomAccessIterator __last, _BidirectionalIterator __result, random_access_iterator_tag, _Distance*)
+{
+    for (_Distance __n = __last - __first; __n > 0; --__n) {
+        *--__result = *--__last;
+    }
+    return __result;
+}
+
+template<typename _BidirectionalIterator1, typename _BidirectionalIterator2, typename _BoolType>
+struct __copy_backward_dispatch
+{
+    typedef typename iterator_traits<_BidirectionalIterator1>::iterator_category    _Cat;
+    typedef typename iterator_traits<_BidirectionalIterator1>::difference_type      _Distance;
+
+    static _BidirectionalIterator2 copy(_BidirectionalIterator1 __first, _BidirectionalIterator1 __last, _BidirectionalIterator2 __result)
+    {
+        return __copy_backward(__first, __last, __result, _Cat(), (_Distance*)(nullptr));
+    }
+};
+
+// 偏特化版本, 两个参数都是 _Tp* 指针形式
+template<typename _Tp>
+struct __copy_backward_dispatch<_Tp*, _Tp*, __true_type>
+{
+    static _Tp* copy(const _Tp* __first, const _Tp* __last, _Tp* __result)
+    {
+        const ptrdiff_t _Num = __last - __first;
+
+    }
+};
+
+
 
 #endif
