b06bca3418a0829daebe2ae2201b6f62f703c88b
yizhikaohongshu <840221557@qq.com>
2025-05-29 11:02:57 +0800

    modified stl_pair.h

--- stl/stl_pair.h
+++ stl/stl_pair.h
@@ -1,62 +1,62 @@
 #ifndef __STL_PAIR_H__
 #define __STL_PAIR_H__
 
-template<typename T1, typename T2>
+template<typename _T1, typename _T2>
 struct pair
 {
-    T1 first;
-    T2 second;
+    _T1 __first;
+    _T2 __second;
 
-    pair() : first(T1()), second(T2()) { }
-    pair(const T1& __a, const T2& __b) : first(__a), second(__b) { }
+    pair() : __first(_T1()), __second(_T2()) { }
+    pair(const _T1& __a, const _T2& __b) : __first(__a), __second(__b) { }
 
-    template<typename U1, typename U2>
-    pair(const pair<U1, U2>& __p) : first(__p.first), second(__p.second) { }
+    template<typename _U1, typename _U2>
+    pair(const pair<_U1, _U2>& __p) : __first(__p.__first), __second(__p.__second) { }
 
-    pair(const pair<T1, T2>& __o) : first(__o.first), second(__o.second) { }
+    pair(const pair<_T1, _T2>& __o) : __first(__o.__first), __second(__o.__second) { }
 };
 
-template<typename T1, typename T2>
-inline bool operator==(const pair<T1, T2>& __x, const pair<T1, T2>& __y)
+template<typename _T1, typename _T2>
+inline bool operator==(const pair<_T1, _T2>& __x, const pair<_T1, _T2>& __y)
 {
-    return __x.first == __y.first && __x.second == __y.second;
+    return __x.__first == __y.__first && __x.__second == __y.__second;
 }
 
-template<typename T1, typename T2>
-inline bool operator<(const pair<T1, T2>& __x, const pair<T1, T2>& __y)
+template<typename _T1, typename _T2>
+inline bool operator<(const pair<_T1, _T2>& __x, const pair<_T1, _T2>& __y)
 {
-    return __x.first < __y.first || 
-            (!(__y.first < __x.first) && __x.second < __y.second);
+    return __x.__first < __y.__first || 
+            (!(__y.__first < __x.__first) && __x.__second < __y.__second);
 }
 
-template<typename T1, typename T2>
-inline bool operator!=(const pair<T1, T2>& __x, const pair<T1, T2>& __y)
+template<typename _T1, typename _T2>
+inline bool operator!=(const pair<_T1, _T2>& __x, const pair<_T1, _T2>& __y)
 {
     return !(__x == __y);
 }
 
-template<typename T1, typename T2>
-inline bool operator>(const pair<T1, T2>& __x, const pair<T1, T2>& __y)
+template<typename _T1, typename _T2>
+inline bool operator>(const pair<_T1, _T2>& __x, const pair<_T1, _T2>& __y)
 {
     return __y < __x;
 }
 
-template<typename T1, typename T2>
-inline bool operator<=(const pair<T1, T2>& __x, const pair<T1, T2>& __y)
+template<typename _T1, typename _T2>
+inline bool operator<=(const pair<_T1, _T2>& __x, const pair<_T1, _T2>& __y)
 {
     return !(__y < __x);
 }
 
-template<typename T1, typename T2>
-inline bool operator>=(const pair<T1, T2>& __x, const pair<T1, T2>& __y)
+template<typename _T1, typename _T2>
+inline bool operator>=(const pair<_T1, _T2>& __x, const pair<_T1, _T2>& __y)
 {
     return !(__x < __y);
 }
 
-template<typename T1, typename T2>
-inline pair<T1, T2> make_pair(const T1& __x, const T2& __y)
+template<typename _T1, typename _T2>
+inline pair<_T1, _T2> make_pair(const _T1& __x, const _T2& __y)
 {
-    return pair<T1, T2>(__x, __y);
+    return pair<_T1, _T2>(__x, __y);
 }
 
 #endif // __STL_PAIR_H__
